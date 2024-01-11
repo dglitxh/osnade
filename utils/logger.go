d6da64647b392bf5530a9bf739929121449b2e7b
ydeezly <ydglitch@gmail.com>
2024-01-11 20:29:44 +0000

    added: logger

---
+++ utils/logger.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"log"
+	"os"
+
+	"log/slog"
+)
+
+func CreateLogFile(name string) *os.File {
+	f, err := os.OpenFile(name+".txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+func Logger() {
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Println("with file/line")
+
+	jsonHandler := slog.NewJSONHandler(os.Stderr, nil)
+	myslog := slog.New(jsonHandler)
+	myslog.Info("hi there")
+
+	myslog.Info("hello again", "key", "val", "age", 25)
+}
